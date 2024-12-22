1380e0acb35eacc686b48715c8e20927ed5810b6
Edu-Fortes <edu.mfortes@gmail.com>
2024-12-21 22:27:35 -0300

    component(list): add list component with calendar integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './components/calendar/calendar.component';
-
+import { ListComponent } from './components/list/list.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalendarComponent],
+  imports: [RouterOutlet, CalendarComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import listPlugin from '@fullcalendar/list';
+
+@Component({
+  selector: 'app-list',
+  imports: [FullCalendarModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  calendarOptions: CalendarOptions = {
+    plugins: [listPlugin],
+    initialView: 'listDay',
+    listDaySideFormat: false,
+    locale: 'pt-br',
+    headerToolbar: false,
+    height: '100%',
+    events: [
+      { title: 'event 1', date: '2024-12-20' },
+      { title: 'event 2', date: '2024-12-20' },
+    ],
+  };
+}
