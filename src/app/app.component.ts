e5610d130eeedaa00f43fc0402139aeed501a610
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-12-26 19:41:03 -0300

    style(app): add Inter font to project and use it in the App.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { ListComponent } from './components/list/list.component';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CalendarComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent { }
