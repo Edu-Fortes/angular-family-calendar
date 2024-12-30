75f492b56c83ca8ff9867b855b505270db9926d7
Edu-Fortes <edu.mfortes@gmail.com>
2024-12-30 08:28:12 -0300

    feat(dialog): enhance CreateDialog to bind event title and implement event creation logic

--- src/app/components/create-dialog/create-dialog.component.ts
+++ src/app/components/create-dialog/create-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, Signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CreateEventDialogStateService } from '../../services/create-event-dialog-state.service';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
@@ -8,13 +9,20 @@ import { SelectionInfoService } from '../../services/selection-info.service';
 
 @Component({
   selector: 'app-create-dialog',
-  imports: [DialogModule, ButtonModule, InputTextModule, FloatLabelModule],
+  imports: [
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabelModule,
+    FormsModule,
+  ],
   templateUrl: './create-dialog.component.html',
   styleUrl: './create-dialog.component.css',
 })
 export class CreateDialogComponent {
   visible;
   selection;
+  eventTitle: string = '';
 
   constructor(
     private dataService: CreateEventDialogStateService,
@@ -31,4 +39,10 @@ export class CreateDialogComponent {
       year: 'numeric',
     });
   });
+
+  createEvent() {
+    console.log(this.eventTitle);
+    this.visible.set(false);
+    this.eventTitle = '';
+  }
 }
