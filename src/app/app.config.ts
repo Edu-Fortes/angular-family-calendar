f3fd949ba40be428ac2db5f95fb8d7a2cf207767
Edu-Fortes <edu.mfortes@gmail.com>
2025-01-06 21:35:05 -0300

    feat(app-config): add localization for month and day names in Portuguese

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura'
+import Aura from '@primeng/themes/aura';
 
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
@@ -15,8 +15,50 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: false || 'none'
-        }
-      }
-    })]
+          darkModeSelector: false || 'none',
+        },
+      },
+      translation: {
+        monthNames: [
+          'Janeiro',
+          'Fevereiro',
+          'Março',
+          'Abril',
+          'Maio',
+          'Junho',
+          'Julho',
+          'Agosto',
+          'Setembro',
+          'Outubro',
+          'Novembro',
+          'Dezembro',
+        ],
+        monthNamesShort: [
+          'Jan',
+          'Fev',
+          'Mar',
+          'Abr',
+          'Mai',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Set',
+          'Out',
+          'Nov',
+          'Dez',
+        ],
+        dayNames: [
+          'Domingo',
+          'Segunda-feira',
+          'Terça-feira',
+          'Quarta-feira',
+          'Quinta-feira',
+          'Sexta-feira',
+          'Sábado',
+        ],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+      },
+    }),
+  ],
 };
